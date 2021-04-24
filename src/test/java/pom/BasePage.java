fc16fa3f3251a55fe63f7c41c2ed9e701670db71
Juan Pablo Bejarano <jbejarano@santandertecnologia.com.ar>
2021-04-24 13:08:06 -0300

    separaciones

--- src/test/java/pom/BasePage.java
+++ src/test/java/pom/BasePage.java
@@ -20,6 +20,7 @@ public class BasePage {
 	}
 
 	public void esperarElemento(By elemento) {
+
 		WebDriverWait wait = new WebDriverWait(driver, 15);
 
 		try {
@@ -37,21 +38,29 @@ public class BasePage {
 	}
 
 	public void elementoClickeable(By elemento) throws Exception {
+
 		try {
-			
+
 			WebDriverWait wait = new WebDriverWait(driver, 15);
-			
+
 			wait.until(ExpectedConditions.elementToBeClickable(elemento));
+
 		} catch (Exception e) {
+
 			Reporte.informar(Status.FAIL, "El elemento: " + elemento + " no es clickeable");
+
 			throw new Exception(e);
+
 		}
 
 	}
 
 	public void scrollToElement(By elemento) {
+
 		WebElement element = driver.findElement(elemento);
+
 		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+
 	}
 
 	public void click(By elemento) throws Exception {
@@ -59,7 +68,9 @@ public class BasePage {
 		try {
 
 			elementoClickeable(elemento);
+
 			driver.findElement(elemento).click();
+
 			Reporte.informar(Status.PASS, "Se hizo click en el elemento: " + elemento);
 
 		} catch (Exception e) {
@@ -74,31 +85,47 @@ public class BasePage {
 		try {
 
 			driver.findElement(elemento).sendKeys(dato);
+
 			Reporte.informar(Status.PASS, "Se envio el dato: " + dato + " al elemento: " + elemento);
 
 		} catch (Exception e) {
+
 			Reporte.informar(Status.FAIL, "No se pudo enviar el dato: " + dato + " al elemento: " + elemento);
+
 		}
 	}
 
 	public boolean isDisplay(By elemento) throws Exception {
+
 		try {
+
 			return driver.findElement(elemento).isDisplayed();
+
 		} catch (Exception e) {
+
 			throw new Exception("No se pudo encontrar el elemento: " + elemento);
+
 		}
 	}
 
 	public String getText(By elemento) throws Exception {
+
 		try {
+
 			return driver.findElement(elemento).getText();
+
 		} catch (Exception e) {
+
 			Reporte.informar(Status.FAIL, "No se pudo tomar el texto del elemento: " + elemento);
+
 			throw new Exception("No se pudo tomar el texto del elemento: " + elemento);
+
 		}
 	}
 
 	public String getTitle() throws Exception {
+
 		return driver.getTitle();
+
 	}
 }
